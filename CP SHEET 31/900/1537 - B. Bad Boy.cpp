e12d3f0bcadb55dbc56bd757525cbcb76655b2e2
Dev Makwana <devmakwana0201@gmail.com>
2025-06-28 21:26:23 +0530

    Create 1537 - B. Bad Boy.cpp

---
+++ CP SHEET 31/900/1537 - B. Bad Boy.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+using namespace std;
+
+void solve() 
+{
+    int n, m, i, j;
+    cin >> n >> m >> i >> j;
+    cout << 1 << " " << 1 << " " << n << " " << m << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    int tc = 1;
+    cin >> tc;
+    while(tc--)
+        solve();
+    return 0;
+}
