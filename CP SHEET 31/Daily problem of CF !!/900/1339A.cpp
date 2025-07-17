a81980ea1201514d788233f20694632b2a93f6e7
Dev Makwana <devmakwana0201@gmail.com>
2025-07-17 23:45:16 +0530

    Create 1339A.cpp

---
+++ CP SHEET 31/Daily problem of CF !!/900/1339A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl "\n"
+using namespace std;
+
+void solve() 
+{
+    int n;
+    cin >> n;
+    cout << n;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc;
+    cin >> tc;
+    while (tc--) solve();
+    return 0;
+}
